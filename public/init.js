c4a61a518df2e4a1b540da0d5b2ff9693fab6e04
nguyenxnguyen113 <65146661+nguyenxnguyen113@users.noreply.github.com>
2020-08-17 10:21:18 +0700

    oke

---
+++ public/init.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+      apiKey: "AIzaSyDiojtqmslSiReejrbBORaCeMnGIZhz86k",
+      authDomain: "video-call-1ac07.firebaseapp.com",
+      databaseURL: "https://video-call-1ac07.firebaseio.com",
+      projectId: "video-call-1ac07",
+      storageBucket: "video-call-1ac07.appspot.com",
+      messagingSenderId: "680270412442",
+      appId: "1:680270412442:web:ec9df53ad3a231ad2b7d23"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
