c0cd50c1bbba0ed5c482c4bfc7efcd9fea6046e0
nguyenxnguyen113 <65146661+nguyenxnguyen113@users.noreply.github.com>
2020-08-17 11:41:15 +0700

    x

--- public/app.js
+++ public/app.js
@@ -2,9 +2,15 @@ mdc.ripple.MDCRipple.attachTo(document.querySelector('.mdc-button'));
 
 const configuration = {
     iceServers: [{
-        urls: [
-            'stun:stun1.l.google.com:19302',
-            'stun:stun2.l.google.com:19302',
+        urls: [{
+                url: 'stun:stun.l.google.com:19302'
+
+            },
+            {
+                url: 'stun:stun1.l.google.com:19302',
+                username: "nguyen456",
+                credential: "123123"
+            }
         ],
     }, ],
     iceCandidatePoolSize: 10,
